eb9d5f160b35a482c523f02b430d97de58f9234c
Mert Güner <mert.guner1@ogr.sakarya.edu.tr>
2018-12-15 14:52:25 +0300

    Add files via upload

---
+++ Source/Algoritma Test/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static Algoritma_Test.RadialBasisFunctionLibrary;
+
+namespace Algoritma_Test
+{
+    public partial class Form1 : Form
+    {
+        Bitmap Destination, BrokenPicture, Final;
+
+        #region Draw bRoken Picture
+        bool DrawEnable = false;
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            DrawEnable = true;
+        }
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (DrawEnable)
+            {
+                if (e.X > -1 && e.Y > -1 && e.X < BrokenPicture.Size.Width && e.Y < BrokenPicture.Size.Height)
+                {
+                    Graphics DrawSurface = Graphics.FromImage(BrokenPicture);
+                    // Set background color to white so that pixels can be correctly colorized
+                    Color value = Destination.GetPixel(e.X, e.Y);
+                    DrawSurface.DrawPolygon(new Pen(value), new Point[] { new Point(e.X, e.Y), new Point(e.X + 1, e.Y), new Point(e.X + 1, e.Y + 1), new Point(e.X, e.Y + 1) });
+
+                    pictureBox2.Image = BrokenPicture;
+                }
+            }
+        }
+
+        private void pictureBox2_MouseLeave(object sender, EventArgs e)
+        {
+            DrawEnable = false;
+        }
+
+        private void pictureBox2_MouseHover(object sender, EventArgs e)
+        {
+            DrawEnable = false;
+        }
+
+        private void pictureBox2_MouseUp(object sender, MouseEventArgs e)
+        {
+            DrawEnable = false;
+        }
+        #endregion
+
+        #region Delegate Functions
+        delegate void GroupBoxText_Change(GroupBox groupBox, string value);
+        private void GroupBoxTextChange(GroupBox groupBox, string value)
+        {
+            try
+            {
+                if (groupBox.InvokeRequired)
+                {
+                    GroupBoxText_Change d = new GroupBoxText_Change(GroupBoxTextChange);
+                    this.Invoke(d);
+                }
+                else
+                {
+                    groupBox.Text = value;
+                }
+            }
+            catch (Exception ex) {  }
+        }
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<HeatPoint> aHeatPoints = new List<HeatPoint>();
+            for (int x = 0; x < BrokenPicture.Size.Width; x++)
+            {
+                for (int y = 0; y < BrokenPicture.Size.Height; y++)
+                {
+                    Color value = BrokenPicture.GetPixel(x,y);                   
+                    if(value.A != 0)
+                    {
+                        Color _color = Destination.GetPixel(x, y);
+                        aHeatPoints.Add(new HeatPoint(x,y, _color));
+                    }
+                }
+            }
+
+            this.BeginInvoke(new GroupBoxText_Change(GroupBoxTextChange), new object[] { groupBox2 , "Broken Picture %" + ((float)aHeatPoints.Count / (float)(BrokenPicture.Size.Width * BrokenPicture.Size.Height) * 100f).ToString("0.0") });
+
+            Final = CreateIntensityMask(Final, aHeatPoints, (float)trackBar1.Value / 100f, (float)trackBar2.Value / 100f);
+
+            pictureBox3.Image = Final;
+        }
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Destination = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            pictureBox1.Image = Destination;
+
+            BrokenPicture = new Bitmap(pictureBox2.Size.Width, pictureBox2.Size.Height);
+            pictureBox2.Image = BrokenPicture;
+
+            Final = new Bitmap(pictureBox3.Size.Width, pictureBox3.Size.Height);
+            pictureBox3.Image = Final;
+
+            Destination = (Bitmap)Bitmap.FromFile(@"Test.bmp");
+
+            pictureBox1.Image = Destination;           
+        }
+    }
+}
---
+++ Source/Algoritma Test/RadialBasisFunctionLibrary.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+
+namespace Algoritma_Test
+{
+    public class RadialBasisFunctionLibrary
+    {
+        public struct HeatPoint
+        {
+            public int X;
+            public int Y;
+            public Color Color;
+            public HeatPoint(int iX, int iY, Color color)
+            {
+                X = iX;
+                Y = iY;
+                Color = color;
+            }
+        }
+
+        public static Color pointValue(Point texturecoordinate, float power, float smoothing, List<HeatPoint> Points)
+        {
+            float[] nominator = new float[4];
+            float denominator = 0;
+            byte[] value = new byte[4];
+
+            for (int i = 0; i < Points.Count; i++)
+            {
+                float dist = 0;
+                if (i < Points.Count) dist = (float)Math.Sqrt(Math.Pow((texturecoordinate.X - Points[i].X), 2) + Math.Pow((texturecoordinate.Y - Points[i].Y), 2) + (smoothing * smoothing));
+                //If the point is really close to one of the data points, return the data point value to avoid singularities
+                if (dist < 0.00000001f)
+                    return Points[i].Color;
+                if (dist.CompareTo(float.NaN) == 0 || dist == 0) return Points[i].Color;
+
+                if (dist.CompareTo(float.NaN) != 0 && dist != 0)
+                {
+                    nominator[0] += (Points[i].Color.R / (float)Math.Pow(dist, power));
+                    nominator[1] += (Points[i].Color.G / (float)Math.Pow(dist, power));
+                    nominator[2] += (Points[i].Color.B / (float)Math.Pow(dist, power));
+                    nominator[3] += (Points[i].Color.A / (float)Math.Pow(dist, power));
+
+                    denominator += (1f / (float)Math.Pow(dist, power));
+                }
+            }
+            //Return NODATA if the denominator is zero
+            if (denominator > 0)
+            {
+                value[0] = Convert.ToByte(Math.Max(0, Math.Min(255, nominator[0] / denominator)));
+                value[1] = Convert.ToByte(Math.Max(0, Math.Min(255, nominator[1] / denominator)));
+                value[2] = Convert.ToByte(Math.Max(0, Math.Min(255, nominator[2] / denominator)));
+                value[3] = Convert.ToByte(Math.Max(0, Math.Min(255, nominator[3] / denominator)));
+            }
+            else value = new byte[4] { 0,0,0,0};
+
+            return Color.FromArgb(value[3], value[0], value[1], value[2]);
+        }
+
+        public static Bitmap CreateIntensityMask(Bitmap bSurface, List<HeatPoint> aHeatPoints, float guc, float yumusat)
+        {
+            // Create new graphics surface from memory bitmap
+            Graphics DrawSurface = Graphics.FromImage(bSurface);
+            // Set background color to white so that pixels can be correctly colorized
+            DrawSurface.Clear(Color.White);
+            // Traverse heat point data and draw masks for each heat point
+            //foreach (HeatPoint DataPoint in aHeatPoints)
+            //{
+            //    // Render current heat point on draw surface
+            //    DrawHeatPoint(DrawSurface, DataPoint, 15);
+            //}
+            Color[,] vals = new Color[bSurface.Size.Width, bSurface.Size.Height];
+            System.Threading.Tasks.Parallel.For(0, vals.GetLength(0), x =>
+            {
+                System.Threading.Tasks.Parallel.For(0, vals.GetLength(1), y =>
+                {
+                    vals[x, y] = pointValue(new Point(x, y), guc, yumusat, aHeatPoints);
+                });
+            });
+
+            for (int x = 0; x < bSurface.Size.Width; x++)
+                for (int y = 0; y < bSurface.Size.Height; y++)
+                    DrawSurface.DrawPolygon(new Pen(vals[x,y]), new Point[] { new Point(x, y), new Point(x + 1, y), new Point(x + 1, y + 1), new Point(x, y + 1) });
+
+            return bSurface;
+        }
+    }
+}
